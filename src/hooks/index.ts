ded6840c124af5c556515586eb0256d414b7c758
Pham Xuan Loc <phamxuanloc443@gmail.com>
2026-09-24 00:48:54 +0700

    chore: delete unused hooks, barrels and component
    useAuth, useDebounce, useMoviePlayer, StatBox and the hooks/services barrels
    had no importers; useAuth also referenced the old synchronous login contract.

--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './useAuth';
-export * from './useWallet';
-export * from './useMoviePlayer';
-export * from './useDebounce';
--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './apiClient';
-export * from './authService';
-export * from './movieService';
-export * from './walletService';
-export * from './subscriptionService';
-export * from './chatService';
-export * from './workflowService';
